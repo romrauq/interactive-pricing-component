4763a07fc5a0f14fe360a450a21218c0b345c61c
romrauq <johnequarmor@gmail.com>
2025-01-22 18:53:17 +0000

    Added scripts that modify and output text content within elements on mobile screens.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,5 @@
 const price_output = document.getElementById("price-output");
+const price_output_mobile = document.getElementById("price-output-mobile");
 const slider_input = document.getElementById("slider-input");
 const toggle_button = document.getElementById("toggle-button");
 
@@ -6,15 +7,18 @@ let is_monthly = true;
 
 // Initial price display
 price_output.innerHTML = `${slider_input.value}`;
+price_output_mobile.innerHTML = `${slider_input.value}`;
 
 // Update price value dynamically using slider input
 slider_input.addEventListener("input", () => {
 	if (is_monthly) {
 		price_output.innerHTML = `${slider_input.value}`;
+		price_output_mobile.innerHTML = `${slider_input.value}`;
 	} else {
 		const discount_amount = Number(slider_input.value) * 0.25;
 		const discounted_price = Number(slider_input.value) - discount_amount;
 		price_output.innerHTML = `${discounted_price.toFixed(2)}`;
+		price_output_mobile.innerHTML = `${discounted_price.toFixed(2)}`;
 	}
 });
 
@@ -25,9 +29,10 @@ toggle_button.addEventListener("click", () => {
 		const discount_amount = Number(slider_input.value) * 0.25;
 		const discounted_price = Number(slider_input.value) - discount_amount;
 		price_output.innerHTML = `${discounted_price.toFixed(2)}`;
+		price_output_mobile.innerHTML = `${discounted_price.toFixed(2)}`;
 	} else {
 		toggle_button.style.justifyContent = "flex-start";
-		price_output.innerHTML = `${slider_input.value}`;
+		price_output_mobile.innerHTML = `${slider_input.value}`;
 	}
 	is_monthly = !is_monthly;
 });
